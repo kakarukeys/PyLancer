f8f579f55f2f6d9ac3b54521c9745bf2daca1ea8
cocbiz <cocbiz@gmail.com>
2012-09-17 10:07:47 +0700

    Add admin for job_board

--- pylancer/settings.py
+++ pylancer/settings.py
@@ -92,10 +92,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
     'south',
     'social_auth',
     'pylancer.apps.job_board',
