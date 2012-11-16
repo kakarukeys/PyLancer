c013c644c232f840a43922e92c46772fbb24b3b5
cocbiz <cocbiz@gmail.com>
2012-11-16 18:36:57 +0700

    Add employer field and add toggle effect

--- pylancer/apps/job_board/models.py
+++ pylancer/apps/job_board/models.py
@@ -16,6 +16,7 @@ class Job(models.Model):
     contract_type = models.CharField(max_length=2, choices=CONTRACT_TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(User)
+    employer = models.CharField(max_length=255)
     is_open = models.BooleanField(default=True)
     location = models.CharField(max_length=40)
     
