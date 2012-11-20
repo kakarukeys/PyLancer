60b835ea3733cc2d7cf75cc063322e0406d79992
Wong Jiang Fung <kakarukeys@gmail.com>
2012-11-20 14:01:40 +0800

    coded in the feature to add job (view, javascript, template) added modal

--- pylancer/apps/job_board/urls.py
+++ pylancer/apps/job_board/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
-from views import JobBoard
+from views import JobBoard, AddJob
 
 urlpatterns = patterns('',
-    url(r'^$', JobBoard.as_view()),
-    
+    url(r'^$', JobBoard.as_view(), name="job_board"),
+    url(r'^add_job/$', AddJob.as_view(), name="add_job")
 )
--- pylancer/apps/job_board/views.py
+++ pylancer/apps/job_board/views.py
@@ -1,25 +1,25 @@
-from django.views.generic.list import BaseListView
-from django.views.generic.base import TemplateView
+import django.views.generic as generic
+from django.core.urlresolvers import reverse_lazy
 
 from models import Job, FreelancerProfile
 
 # Create your views here.
 
-class JobList(BaseListView):
+class JobList(generic.list.BaseListView):
     """A list of jobs"""
     model = Job
     
     def render_to_response(self, context):
         return context  #not rendering any template, the context is used in another view.
         
-class FreelancerProfileList(BaseListView):
+class FreelancerProfileList(generic.list.BaseListView):
     """A list of freelancer profiles"""
     model = FreelancerProfile
     
     def render_to_response(self, context):
         return context
         
-class JobBoard(TemplateView):
+class JobBoard(generic.base.TemplateView):
     template_name = "job_board/job_board.html"
     
     def __init__(self, **kwargs):
@@ -37,3 +37,9 @@ class JobBoard(TemplateView):
         context["freelancer_profile_list"] = self.freelancer_profile_list_view(request, *args, **kwargs)
         return self.render_to_response(context)
         
+class AddJob(generic.edit.CreateView):
+    model = Job
+    initial = {"title": "Ninja Programmer"}
+    success_url = reverse_lazy("job_board") #after adding, redirect to job board paeg
+    template_name = "job_board/add_job.html"
+    
